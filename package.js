bef9a547115fa1d3cfa47b4e0d7d100612a7f484
zocky <zocky@naoknu.(none)>
2013-11-26 05:21:25 +0100

    rename files

--- package.js
+++ package.js
@@ -6,8 +6,7 @@ Package._transitional_registerBuildPlugin({
   name: "pencil-templates",
   use: ['templating','spark'],
   sources: [
-    "plugin/pencil.js",
-    "plugin/template_scanner.js",
+    "plugin/pencil-parser.js",
     "plugin/compile-templates.js"
   ],
 });
@@ -15,7 +14,7 @@ Package._transitional_registerBuildPlugin({
 // This on_use describes the *runtime* implications of using this package.
 Package.on_use(function (api) {
   api.imply(['meteor','spark','handlebars'], 'client');
-  api.add_files(['runtime.js'],'client');
+  api.add_files(['pencil-runtime.js'],'client');
   api.export('Pencil');
 });
 
